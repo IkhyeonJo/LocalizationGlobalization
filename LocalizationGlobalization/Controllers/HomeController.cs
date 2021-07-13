06e5a53dcdfed8609b898a35e91b6665f739d834
IkHyeonJo <joeun3518@gmail.com>
2021-07-13 16:34:44 +0900

    Add: Culture Button

--- LocalizationGlobalization/Controllers/HomeController.cs
+++ LocalizationGlobalization/Controllers/HomeController.cs
@@ -36,12 +36,12 @@ namespace LocalizationGlobalization.Controllers
         }
 
         [HttpPost]
-        public IActionResult CultureManagement(string culture)
+        public IActionResult CultureManagement(string culture, string returnUrl)
         {
             Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
                 new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
 
-            return RedirectToAction(nameof(Index));
+            return LocalRedirect(returnUrl);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
